0482e445e6a8412a7eb9bc9f29cb01fea65b3ca7
santacrab2 <79347566+santacrab2@users.noreply.github.com>
2022-06-04 09:11:32 -0400

    Another hacker caught

--- Bots/GTSBot.cs
+++ Bots/GTSBot.cs
@@ -15,7 +15,7 @@ namespace _3DS_link_trade_bot
     public class GTSBot
     {
         
-        public static List<string> KnownGTSBreakers = new List<string> { "funkygamer26", "chloegarcia", "volcano.“do”", "33012888", "edou", "moon.", "unknown.yt", "japan.kebuju", "はちゆきおし", "あああ" , "あか", "adventrsnivy", "noxii",""," ", "doudou#6666", "doudou#9999", "zeraoratv=yt", "sun.", "leonmaxi.tv", "rayky", "kewl", "not toxi", "serkan", "arceus", "flecheringyt" }; 
+        public static List<string> KnownGTSBreakers = new List<string> { "funkygamer26", "chloegarcia", "volcano.“do”", "33012888", "edou", "moon.", "unknown.yt", "japan.kebuju", "はちゆきおし", "あああ" , "あか", "adventrsnivy", "noxii",""," ", "doudou#6666", "doudou#9999", "zeraoratv=yt", "sun.", "leonmaxi.tv", "rayky", "kewl", "not toxi", "serkan", "arceus", "flecheringyt", "quit the gts" }; 
         public static int tradeindex;
         public static int gtspagesize;
         
